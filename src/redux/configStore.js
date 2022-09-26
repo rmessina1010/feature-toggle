be61710b277a00a2c8812faa56b9657cb590c1c4
Ray Messina <rmessina10@gmail.com>
2022-09-26 00:01:30 -0500

    installs redux-state-sync to manage session accross tabs

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,22 +1,32 @@
 import { persistStore, persistReducer } from 'redux-persist'
 import sessionStorage from 'redux-persist/lib/storage/session'
 
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
-import { createStore } from 'redux';
+// import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { createStore, applyMiddleware } from 'redux';
+
+import { createStateSyncMiddleware, initMessageListener, initStateWithPrevTab
+} from "redux-state-sync";
+
 import toggles from '../shared/toggles';
 
 import rootReducer  from './reducers';
 
 const persistConfig = {
   key: 'root',
-  storage:sessionStorage
+  storage : sessionStorage
 }
 
 const ConfigureStore = () => {
       const persistedReducer = persistReducer(persistConfig, rootReducer)
-      const store = createStore(persistedReducer, {toggles})
+      const store = createStore(
+        persistedReducer,
+        {toggles},
+        applyMiddleware(createStateSyncMiddleware({blacklist: ["persist/PERSIST", "persist/REHYDRATE"],}))
+      )
       const persistor = persistStore(store)
       return { persistor, store };
 }
 
 export const { persistor, store } = ConfigureStore();
+
+initStateWithPrevTab(store);
